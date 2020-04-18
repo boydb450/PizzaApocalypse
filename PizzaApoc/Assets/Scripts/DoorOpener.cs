fb0f635d6e661d5ec903caf8c4f3bb21e6ecf47d
boydb450 <62711970+boydb450@users.noreply.github.com>
2020-04-18 14:25:25 -0400

    Door Animation & reflections
    animated the door opening (in code), and added a reflection probe to enchance reflections, and baked in a new lightmap to show this

---
+++ PizzaApoc/Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour
+{
+
+    public GameObject hinge;
+    public GameObject door;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (hinge.transform.localRotation.y < 0.8f) { OpenDoor(); }
+
+        }
+    }
+
+    private void OpenDoor()
+    {
+        print(hinge.transform.localRotation.y);
+        hinge.transform.Rotate(new Vector3(0,30,0), 50 * Time.deltaTime);
+        
+    }
+
+}
