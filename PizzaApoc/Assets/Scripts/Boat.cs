b8a9df86d671b73aa844d250ec4476b2072a2032
boydb450 <62711970+boydb450@users.noreply.github.com>
2020-04-22 16:11:16 -0400

    Vehicles
    Added vehicle system

---
+++ PizzaApoc/Assets/Scripts/Boat.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Boat : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    [SerializeField] GameObject Barrier;
+    [SerializeField] GameObject NoReturn;
+    AudioSource feetstampers;
+    FirstPersonController playervariables;
+    [SerializeField] Quaternion boatAngle;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playervariables = Player.GetComponent<FirstPersonController>();
+        feetstampers = Player.GetComponent<AudioSource>();
+        NoReturn.SetActive(false);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            feetstampers.enabled = false;
+            gameObject.transform.parent = Player.transform;
+            gameObject.transform.localRotation = boatAngle;
+            gameObject.transform.localPosition = new Vector3(0.1f, -0.75f, 0.73f);
+            playervariables.m_RunSpeed = 5;
+            playervariables.m_UseHeadBob = false;
+            playervariables.m_GravityMultiplier = 9999;
+            Barrier.SetActive(false);
+            NoReturn.SetActive(true);
+        }
+    }
+}
---
+++ PizzaApoc/Assets/Scripts/Boat1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Boat1 : MonoBehaviour
+{
+    [SerializeField] GameObject Player;
+    [SerializeField] GameObject Barrier;
+    AudioSource feetstampers;
+    FirstPersonController playervariables;
+    [SerializeField] Quaternion boatAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playervariables = Player.GetComponent<FirstPersonController>();
+        feetstampers = Player.GetComponent<AudioSource>();
+
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            feetstampers.enabled = false;
+            gameObject.transform.parent = Player.transform;
+            gameObject.transform.localRotation = boatAngle;
+            gameObject.transform.localPosition = new Vector3(0.1f, -0.66f, 0.4f);
+            playervariables.m_RunSpeed = 15;
+            playervariables.m_WalkSpeed = 10;
+            playervariables.m_UseHeadBob = false;
+            playervariables.m_GravityMultiplier = 9999;
+            Barrier.SetActive(false);
+        }
+    }
+}
