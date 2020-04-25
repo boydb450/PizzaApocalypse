70a1745282a3b0d89929f0addd21818a3c058939
boydb450 <62711970+boydb450@users.noreply.github.com>
2020-04-24 20:00:12 -0400

    Added Menus
    Added the controls menu and made controls and main menu interactive

---
+++ PizzaApoc/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+
+
+    public void controls()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void goBack()
+    {
+        SceneManager.LoadScene("Start Menu");
+    }
+}
