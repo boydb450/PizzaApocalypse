0176c3f27813f76e22b611076be36796f6fa9bf4
boydb450 <62711970+boydb450@users.noreply.github.com>
2020-04-27 21:52:03 -0400

    Fixed pizza box system

---
+++ PizzaApoc/Assets/Scripts/BoxGetta.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxGetta : MonoBehaviour
+{
+    public bool boxy;
+    public Quaternion baoal;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameObject.transform.parent = other.transform;
+            gameObject.transform.localPosition = new Vector3(0, 0, 0);
+            gameObject.transform.localRotation = baoal;
+            boxy = true;
+        }
+    }
+}
--- PizzaApoc/Assets/Scripts/DoorOpener.cs
+++ PizzaApoc/Assets/Scripts/DoorOpener.cs
@@ -8,10 +8,17 @@ public class DoorOpener : MonoBehaviour
 
     public GameObject hinge;
     public GameObject door;
+    public GameObject box;
+    BoxGetta refScript;
+
+    private void Start()
+    {
+        refScript = box.GetComponent<BoxGetta>();
+    }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") & refScript.boxy)
         {
             if (hinge.transform.localRotation.y < 0.8f) { OpenDoor(); }
 
